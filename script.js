bcda5f2d7cdd321cef265b6a0a4f1439f4aacd71
Josu Alzueta <josu.alzueta@biko2.com>
2024-04-10 22:37:58 +0200

    Adds update method and adds click listeners to checks

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ console.log("Añadamos a algún enemigo...");
 const form = document.getElementById("form");
 form.addEventListener("submit", onFormSubmit);
 
+document
+  .querySelectorAll('input[type="checkbox"]')
+  .forEach(function (itemCheck) {
+    itemCheck.addEventListener("click", function () {
+      updateStatusPills();
+    });
+  });
+
+function updateStatusPills() {
+  const totalItems = document.querySelectorAll('input[type="checkbox"]').length;
+  const doneItems = document.querySelectorAll(
+    'input[type="checkbox"]:checked'
+  ).length;
+  const pendingItems = totalItems - doneItems;
+
+  const allItemsPill = document.getElementById("all-items-pill");
+  allItemsPill.innerText = totalItems;
+  const doneItemsPill = document.getElementById("done-items-pill");
+  doneItemsPill.innerText = doneItems;
+  const pendingItemsPill = document.getElementById("pending-items-pill");
+  pendingItemsPill.innerText = pendingItems;
+}
+
 function addListItem(name) {
   if (!name) return;
 
@@ -15,6 +38,9 @@ function addListItem(name) {
 
   const checkInput = document.createElement("input");
   checkInput.type = "checkbox";
+  checkInput.addEventListener("click", function () {
+    updateStatusPills();
+  });
 
   label.appendChild(checkInput);
 
@@ -24,6 +50,8 @@ function addListItem(name) {
   list.appendChild(listItem);
 
   document.getElementById("nombre").value = "";
+
+  updateStatusPills();
 }
 
 function onFormSubmit(event) {
